5427007eaeffb9bad0d8cb0bf11a88d0e13cc7c5
Yuandika Alfahreiza <yuandika11@gmail.com>
2021-12-09 13:29:15 +0700

    Latihan Navigation Drawer dengan NavigationComponent Tujuan

--- app/src/main/java/com/dicoding/mynavigationdrawer/MainActivity.kt
+++ app/src/main/java/com/dicoding/mynavigationdrawer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dicoding.mynavigationdrawer
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -32,7 +33,9 @@ class MainActivity : AppCompatActivity() {
 
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+                .setAction("Action") {
+                    Toast.makeText(this@MainActivity, "Halo ini action dari snackbar", Toast.LENGTH_SHORT).show()
+                }.show()
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
@@ -49,7 +52,11 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_cart
             ), drawerLayout
         )
+        //setupActionBarWithNavController digunakan untuk mengatur judul AppBar
+        // agar sesuai dengan Fragment yang ditampilkan.
         setupActionBarWithNavController(navController, appBarConfiguration)
+        //Dan yang terakhir, setupWithNavController digunakan supaya NavigationDrawer
+        // menampilkan Fragment yang sesuai ketika menu dipilih.
         navView.setupWithNavController(navController)
     }
 
@@ -59,6 +66,7 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    //mengatur ketika tombol back ditekan
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
